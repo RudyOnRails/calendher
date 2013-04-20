b101cfa3bbc8b43ba6c9e2c0346ad56c8701eaf9
Kevin Musiorski <rudyonrails@gmail.com>
2013-04-20 10:12:58 -0500

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Calendher::Application.config.secret_token = '6e45de0b1fcf381fbb13096b4bb8e78e3d26b893080aa84444da51572ab635a80e22caeedd345425efc64d610c9daac736f42273617d5bdae7142f25de4a6ebb'
