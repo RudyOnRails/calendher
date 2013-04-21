c76531d3655ad2860692b795525138a3fe7ce4c2
Kevin Musiorski <rudyonrails@gmail.com>
2013-04-21 01:19:41 -0500

    setting up sendgrid for prod

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,6 +3,7 @@ class UserMailer < ActionMailer::Base
 
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: "Thanks for signing up!")
+    @from = "\"Calendher\" <welcome@calendher.com>"
+    mail(from: @from, to: @user.email, subject: "Thanks for signing up!")
   end
 end
